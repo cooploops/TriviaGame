932df7b79a08e9ec0b96eb1287e5876dbd0e0b14
TJ <cooploops@gmail.com>
2017-09-20 00:59:16 -0700

    add more style in css and js with images and background colors and borders

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,8 @@ var game = {
 	correct: 0,
 	incorrect: 0,
 	questionTimer: 45,
-	postAnswerTimeOut: 7000,
+	postAnswerTimeOut: 10000,
+	gameImage: "",
 	currentQuestion: "",
 	correctAns: "",
 	incorrect1: "",
@@ -23,85 +24,97 @@ var game = {
 			wrongans1: "because the ocean reflects the the color of blue into the sky",
 			wrongans2: "because that's just how Earth's sky is",
 			wrongans3: "because it has something to do with light",
-			rightans: "because of Rayleigh Scattering"
+			rightans: "because of Rayleigh Scattering",
+			image: "assets/images/rayleighScattering.jpg"
+
 		},
 		{
 			ques: "What color of light does the sun give off?",
 			wrongans1: "yellow!",
 			wrongans2: "yellow-ish orange!",
-			wrongans3: "redish purple at sunrise and sunset then yellow during the day",
-			rightans: "white!"
+			wrongans3: "reddish purple at sunrise and sunset then yellow during the day",
+			rightans: "white!",
+			image: "assets/images/whiteLight.gif"
 		},
 		{
 			ques: "Why are black holes called black holes?",
 			wrongans1: "because they're black in color",
 			wrongans2: "because everything they suck in just mixes up into 'black' matter",
 			wrongans3: "because it sounds cool and less threatening that 'end-of-reality' hole",
-			rightans: "they're actually the brightest objects in space but their light never reaches our eyes because of their own immense gravity"
+			rightans: "they're actually the brightest objects in space but their light never reaches our eyes because of their own immense gravity, which makes them appear as black spots in the sky",
+			image: "assets/images/blackHoleDiagram.jpg"
 		},
 		{
 			ques: "What is light?",
 			wrongans1: "it's just light",
 			wrongans2: "it's a wavelength on the electromagnetic spectrum",
 			wrongans3: "it's a a super tiny particle we can't see",
-			rightans: "it's both a wavelength and a particle"
+			rightans: "it's both a wavelength and a particle",
+			image: "assets/images/lightWaveParticle.jpg"
 		},
 		{
-			ques: "Why are plants green, cars red, and my jeans blue?",
+			ques: "Why do we see plants as green, cars as red, and jeans as blue?",
 			wrongans1: "because that's what they were colored",
 			wrongans2: "because they give off that color of light",
 			wrongans3: "plants are naturally green, cars are painted red, and jeans are dyed blue",
-			rightans: "because we see colors that are reflected off of objects"
+			rightans: "because we see the colors that's reflected off of those objects",
+			image: "assets/images/greenLightReflected.png"
 		},
 		{
 			ques: "Which of the following colors has the lowest frequency out of visible light?",
 			wrongans1: "violet",
 			wrongans2: "orange",
 			wrongans3: "ultraviolet",
-			rightans: "red"
+			rightans: "red",
+			image: "assets/images/visibleLightWavelengths.gif"
 		},
 		{
 			ques: "How do rainbows work?",
 			wrongans1: "they reflect light off the clouds",
 			wrongans2: "water is reflecting the suns light",
 			wrongans3: "the clouds are blocking some light and only letting the colors of the rainbow through",
-			rightans: "water droplets in the sky are diffracting the light from the sun"
+			rightans: "water droplets in the sky are diffracting the light from the sun",
+			image: "assets/images/lightDiffraction.gif"
 		},
 		{
-			ques: "How long does light take to get from the sun to the earth",
+			ques: "How long does light take to get from the sun to the earth?",
 			wrongans1: "instantly",
 			wrongans2: "between 3 to 5 minutes",
 			wrongans3: "about 5 seconds",
-			rightans: "between 8 to 10 minutes"
+			rightans: "between 8 to 10 minutes",
+			image: "assets/images/mindBlowngif.gif"
 		},
 		{
 			ques: "What are radio waves?",
 			wrongans1: "they're sound waves made by radio stations",
 			wrongans2: "tiny reverberations in the air that we use antennae to receive and intensify the signal so we can listen",
 			wrongans3: "they're electricty that we shoot from antennae to antennae",
-			rightans: "they're electromagnetic waves that travel in all directions"
+			rightans: "they're electromagnetic waves that travel in all directions",
+			image: "assets/images/radioWave.gif"
 		},
 		{
 			ques: "what makes fiber optic wires so fast?",
 			wrongans1: "becase they use carbon-fiber instead of regular metal",
 			wrongans2: "because their wires don't have any bends and lets things travel faster in a straight line",
 			wrongans3: "because they shoot lasers that travel faster than normal electricity",
-			rightans: "because they use total internal reflection to make light reflect back in on itself"
+			rightans: "because they use total internal reflection to make light reflect back in on itself",
+			image: "assets/images/totalInternalReflection.jpg"
 		}
 		],
 
 		// functions to call
 		generateQuestion: function() {
-			var index = Math.floor((Math.random() * (game.questions.length-1))+0.99);
-			console.log(index);
-			this.currentQuestion = game.questions[index].ques;
-			this.correctAns = game.questions[index].rightans;
-			this.incorrect1 = game.questions[index].wrongans1;
-			this.incorrect2 = game.questions[index].wrongans2;
-			this.incorrect3 = game.questions[index].wrongans3;
+			Index = Math.floor((Math.random() * (game.questions.length-1))+0.99);
+			console.log(Index);
+			this.currentQuestion = game.questions[Index].ques;
+			this.correctAns = game.questions[Index].rightans;
+			this.incorrect1 = game.questions[Index].wrongans1;
+			this.incorrect2 = game.questions[Index].wrongans2;
+			this.incorrect3 = game.questions[Index].wrongans3;
+			this.gameImage = game.questions[Index].image;
 			this.possAns = [game.correctAns, game.incorrect1, game.incorrect2, game.incorrect3];
 			game.possAnsLength = this.possAns.length;
-			this.questionsUsed.push(this.questions.splice(index,1));
+			this.questionsUsed.push(this.questions.splice(Index,1));
 		},
 
 		generateAnswers: function() {
@@ -113,6 +126,11 @@ var game = {
 				answerDiv.addClass("answerDiv");
 				answerDiv.text(game.possAns[index]);
 				$("#questionDiv").append(answerDiv);
+				$(".answerDiv").hover(function(){
+					$(this).addClass("answerHover");
+						},function(){
+					$(this).removeClass("answerHover");
+				});
 				game.possAns.splice(index,1);
 				game.possAnsLength = game.possAns.length;
 			}
@@ -137,7 +155,7 @@ var game = {
 
 		postQuestionScreen: function() {
 			// set delay before changing screen
-			var answerDelay = setTimeout(function(){
+			setTimeout(function(){
 				game.clickRecording = false;
 				game.reset();
 				$(".answerDiv").css("display","block");
@@ -157,7 +175,8 @@ var game = {
 				// show wins and losses and ask if they want to play again
 				$("#questionDiv").css("display","none");
 				var results = $("<div>");
-				results.html("<p class='lead'>Here are your results! Feel free to play again if you'd like by refreshing the page!</p> <br> <span>correct: " + game.correct + "</span> <br> <span>incorrect: " + game.incorrect + "</span>")
+				results.addClass("text-center");
+				results.html("<p class='lead'>Here are your results! Feel free to play again if you'd like by refreshing the page!</p> <br> <span id='endScore'>correct: " + game.correct + "</span> <br> <span id='endScore'>incorrect: " + game.incorrect + "</span>")
 				$("div.jumbotron").append(results);
 			}
 		}
@@ -192,19 +211,18 @@ $(window).click(function(event){
 	if(!game.clickRecording){
 		console.log(game.clickRecording);
 		game.click = event.target.innerHTML;
-		$(".answerDiv").hover(function(){
-			$(this).addClass("answerHover");
-		},function(){
-			$(this).removeClass("answerHover");
-		});
 		// if correct answer chosen increase correct counter by 1 and show postAnswer screen stuff 
 		if(game.click === game.correctAns){
 			$(".answerDiv").css("display", "none");
 			var postAnswer = $("<div>");
+			var postAnswerImage = $("<img>");
 			postAnswer.addClass("postAnswer");
 			postAnswer.addClass("text-center");
-			postAnswer.html("<p>correct!</p>")
+			postAnswer.html("<p>Correct!</p>");
+			postAnswerImage.attr("src", game.gameImage);
+			postAnswerImage.addClass("answerImage");
 			$("#questionDiv").append(postAnswer);
+			$("#questionDiv").append(postAnswerImage);
 			game.correct++;
 			clearInterval(game.interval);
 			console.log("correct!");
@@ -214,10 +232,14 @@ $(window).click(function(event){
 		} else if(game.click === game.incorrect1 || game.click === game.incorrect2 || game.click === game.incorrect3){
 			$(".answerDiv").css("display", "none");
 			var postAnswer = $("<div>");
+			var postAnswerImage = $("<img>");
 			postAnswer.addClass("postAnswer");
 			postAnswer.addClass("text-center");
 			postAnswer.html("<p>I'm sorry that's incorrect. The correct answer is " + game.correctAns + "</p>")
+			postAnswerImage.attr("src", game.gameImage);
+			postAnswerImage.addClass("answerImage");
 			$("#questionDiv").append(postAnswer);
+			$("#questionDiv").append(postAnswerImage);
 			clearInterval(game.interval);
 			game.incorrect++;
 			game.clickRecording = true;
